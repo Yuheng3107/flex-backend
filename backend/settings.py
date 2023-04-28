1979089023b1a04da16e47dc6e79f09ed0952389
Kuang Yu Heng <yuheng3107@gmail.com>
2023-04-28 09:22:02 +0800

    Made csrf allow all origins

--- backend/settings.py
+++ backend/settings.py
@@ -175,7 +175,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOW_ALL_ORIGINS = True
-CSRF_TRUSTED_ORIGINS = ['http://localhost:3000', 'http://localhost:8000']
+CSRF_TRUSTED_ORIGINS = ["*"]
 
 # Custom User Model
 AUTH_USER_MODEL = 'users.AppUser'
