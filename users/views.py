7d7250c09c3303c180b812197e698a60c85a3d3e
Kuang Yu Heng <yuheng3107@gmail.com>
2023-04-29 15:31:11 +0800

    Made minor edits

--- users/views.py
+++ users/views.py
@@ -4,14 +4,16 @@ from django.contrib.auth import get_user_model, login
 from django.http import HttpResponse
 from django.middleware.csrf import get_token
 from .serializer import UserSerializer, OtherUserSerializer
-from achievements.models import Achievement #type: ignore
-from community.models import Community #type: ignore
-from exercises.models import Exercise, ExerciseRegime #type: ignore
-from chat.models import ChatGroup #type: ignore
+from achievements.models import Achievement  # type: ignore
+from community.models import Community  # type: ignore
+from exercises.models import Exercise, ExerciseRegime  # type: ignore
+from chat.models import ChatGroup  # type: ignore
 from rest_framework.parsers import FormParser, MultiPartParser
 from django.db.models import Count
 
 User = get_user_model()
+
+
 class UserCreateView(APIView):
     def post(self, request):
         """API used internally to create users in DB from social logins"""
@@ -38,6 +40,7 @@ class UserCreateView(APIView):
             response.write("User Successfully Registered")
         return response
 
+
 class UserAllowedView(APIView):
     def post(self, request):
         if "username" not in request.data or "email" not in request.data:
@@ -46,7 +49,8 @@ class UserAllowedView(APIView):
         if User.objects.filter(email=request.data["email"]).exists() or User.objects.filter(username=request.data["username"]).exists():
             return Response(False)
         return Response(True)
-    
+
+
 class UserDetailView(APIView):
     def get(self, request):
         if (request.user.is_authenticated):
@@ -54,6 +58,7 @@ class UserDetailView(APIView):
             return Response(serializer.data)
         else:
             return Response(False, status=status.HTTP_401_UNAUTHORIZED)
+
 
 class UserOthersDetailView(APIView):
     def get(self, request, pk):
@@ -63,7 +68,8 @@ class UserOthersDetailView(APIView):
             serializer = OtherUserSerializer(other_user)
             return Response(serializer.data)
         except:
-            return Response(status=status.HTTP_400_BAD_REQUEST) 
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+
 
 class UserOthersListView(APIView):
     def post(self, request):
@@ -74,11 +80,13 @@ class UserOthersListView(APIView):
         users = User.objects.filter(pk__in=request.data["user_ids"])
         serializer = OtherUserSerializer(users, many=True)
         return Response(serializer.data)
-        
+
+
 class CheckLoginStatus(APIView):
     def get(self, request):
         return Response(request.user.is_authenticated)
-    
+
+
 class UserUpdateView(APIView):
     def post(self, request):
         """Post normal fields here"""
@@ -87,23 +95,25 @@ class UserUpdateView(APIView):
         if (len(request.data) == 0 or "id" in request.data):
             return Response(status=status.HTTP_400_BAD_REQUEST)
         fields = ["username", "privacy_level", "email", "bio"]
-        fields = {field: request.data[field] for field in fields if field in request.data}
+        fields = {field: request.data[field]
+                  for field in fields if field in request.data}
         User = get_user_model()
         # Update the user with the new fields
         try:
             User.objects.filter(pk=request.user.id).update(**fields)
         except Exception as e:
             return Response(str(e), status=status.HTTP_400_BAD_REQUEST)
-        
+
         return Response()
+
 
 class UserUpdateProfilePhotoView(APIView):
     def post(self, request):
         parser_classes = [FormParser, MultiPartParser]
-        
+
         if not request.user.is_authenticated:
             return Response(status=status.HTTP_401_UNAUTHORIZED)
-        
+
         uploaded_file_object = request.FILES.get("photo", None)
         # Check that profile photo is indeed uploaded
         if uploaded_file_object is None:
@@ -123,9 +133,11 @@ class UserUpdateProfilePhotoView(APIView):
         user.profile_photo = uploaded_file_object
         user.save()
         return Response()
-        
+
+
 class UserManyToManyUpdateView(APIView):
     """Base class to update m2m relationships for users"""
+
     def setup(self, request, *args, **kwargs):
         # Model is the model of the object with m2m relationship with users
         self.model = None
@@ -133,7 +145,7 @@ class UserManyToManyUpdateView(APIView):
         self.field_name = None
         # These two attributes will need to be overwritten in the descendant class
         return super().setup(self, request, *args, **kwargs)
-    
+
     def post(self, request):
         """Adds new m2m relationships to user model"""
         # User must be authenticated
@@ -142,17 +154,19 @@ class UserManyToManyUpdateView(APIView):
         # Checks that there is a model setup
         if self.model is None or self.field_name is None:
             return Response(status=status.HTTP_400_BAD_REQUEST)
-        
+
         if "fk_list" not in request.data:
             return Response("Please add list of foreign keys as fk_list in request.", status=status.HTTP_400_BAD_REQUEST)
-       
+
         # Adds the relations to the model
         try:
             # Unpacks foreign keys in fk_list
-            getattr(request.user, self.field_name).add(*request.data["fk_list"])
+            getattr(request.user, self.field_name).add(
+                *request.data["fk_list"])
             return Response()
         except Exception as e:
             return Response(str(e), status=status.HTTP_400_BAD_REQUEST)
+
 
 class UserAchievementsUpdateView(UserManyToManyUpdateView):
     def setup(self, request, *args, **kwargs):
@@ -160,24 +174,28 @@ class UserAchievementsUpdateView(UserManyToManyUpdateView):
         self.model = Achievement
         self.field_name = 'achievements'
 
+
 class UserBlockedUpdateView(UserManyToManyUpdateView):
     def setup(self, request, *args, **kwargs):
         super().setup(request, *args, **kwargs)
         self.model = get_user_model()
         self.field_name = 'blocked'
-        
+
+
 class UserFollowingUpdateView(UserManyToManyUpdateView):
     def setup(self, request, *args, **kwargs):
         super().setup(request, *args, **kwargs)
         self.model = get_user_model()
         self.field_name = 'following'
-        
+
+
 class UserCommunitiesUpdateView(UserManyToManyUpdateView):
     def setup(self, request, *args, **kwargs):
-        
+
         super().setup(request, *args, **kwargs)
         self.model = Community
         self.field_name = 'communities'
+
     def post(self, request, *args, **kwargs):
         response = super().post(request, *args, **kwargs)
         # Let super method validate data sent, only update member_count if it adding m2m is successful
@@ -189,28 +207,32 @@ class UserCommunitiesUpdateView(UserManyToManyUpdateView):
             pk = pk_list[0]
             update_community_member_count(1, pk)
         return response
-            
-        
+
+
 class UserExercisesUpdateView(UserManyToManyUpdateView):
     def setup(self, request, *args, **kwargs):
         super().setup(request, *args, **kwargs)
         self.model = Exercise
         self.field_name = 'exercises'
 
+
 class UserExerciseRegimesUpdateView(UserManyToManyUpdateView):
     def setup(self, request, *args, **kwargs):
         super().setup(request, *args, **kwargs)
         self.model = ExerciseRegime
         self.field_name = 'exercise_regimes'
-        
+
+
 class UserChatGroupsUpdateView(UserManyToManyUpdateView):
     def setup(self, request, *args, **kwargs):
         super().setup(request, *args, **kwargs)
         self.model = ChatGroup
         self.field_name = 'chat_groups'
 
+
 class UserManyToManyDeleteView(APIView):
     """Base class to delete m2m relationships for users"""
+
     def setup(self, request, *args, **kwargs):
         # Model is the model of the object with m2m relationship with users
         self.model = None
@@ -218,7 +240,7 @@ class UserManyToManyDeleteView(APIView):
         self.field_name = None
         # These two attributes will need to be overwritten in the descendant class
         return super().setup(self, request, *args, **kwargs)
-    
+
     def delete(self, request, pk):
         """Delete m2m relationship"""
         # Make sure user is authenticated
@@ -233,75 +255,87 @@ class UserManyToManyDeleteView(APIView):
         except Exception as e:
             return Response(str(e), status=status.HTTP_400_BAD_REQUEST)
 
+
 class UserAchievementsDeleteView(UserManyToManyDeleteView):
     def setup(self, request, *args, **kwargs):
         super().setup(request, *args, **kwargs)
         self.model = Achievement
         self.field_name = 'achievements'
-        
+
+
 class UserFriendRequestDeleteView(UserManyToManyDeleteView):
     def setup(self, request, *args, **kwargs):
         super().setup(request, *args, **kwargs)
         self.model = get_user_model()
         self.field_name = 'sent_friend_requests'
 
+
 class UserBlockedDeleteView(UserManyToManyDeleteView):
     def setup(self, request, *args, **kwargs):
         super().setup(request, *args, **kwargs)
         self.model = get_user_model()
         self.field_name = 'blocked'
-        
+
+
 class UserFollowingDeleteView(UserManyToManyDeleteView):
     def setup(self, request, *args, **kwargs):
         super().setup(request, *args, **kwargs)
         self.model = get_user_model()
         self.field_name = 'following'
-        
+
+
 class UserCommunitiesDeleteView(UserManyToManyDeleteView):
     def setup(self, request, *args, **kwargs):
         super().setup(request, *args, **kwargs)
         self.model = Community
         self.field_name = 'communities'
+
     def delete(self, request, pk, *args, **kwargs):
         response = super().delete(request, pk, *args, **kwargs)
         # Let super method validate data sent, only update member_count if removing m2m is successful
         if response.status_code != status.HTTP_200_OK:
             return response
-        update_community_member_count(-1, pk)    
+        update_community_member_count(-1, pk)
         return response
-        
+
+
 class UserExercisesDeleteView(UserManyToManyDeleteView):
     def setup(self, request, *args, **kwargs):
         super().setup(request, *args, **kwargs)
         self.model = Exercise
         self.field_name = 'exercises'
 
+
 class UserExerciseRegimesDeleteView(UserManyToManyDeleteView):
     def setup(self, request, *args, **kwargs):
         super().setup(request, *args, **kwargs)
         self.model = ExerciseRegime
         self.field_name = 'exercise_regimes'
-        
+
+
 class UserChatGroupsDeleteView(UserManyToManyDeleteView):
     def setup(self, request, *args, **kwargs):
         super().setup(request, *args, **kwargs)
         self.model = ChatGroup
         self.field_name = 'chat_groups'
-        
+
+
 class UserFollowingListView(APIView):
     def get(self, request):
         if not request.user.is_authenticated:
             return Response(status=status.HTTP_401_UNAUTHORIZED)
         serializer = UserSerializer(request.user.following.all(), many=True)
         return Response(serializer.data)
-    
+
+
 class UserFollowerListView(APIView):
     def get(self, request):
         if not request.user.is_authenticated:
             return Response(status=status.HTTP_401_UNAUTHORIZED)
         serializer = UserSerializer(request.user.followers.all(), many=True)
         return Response(serializer.data)
-    
+
+
 class UserStreakUpdateView(APIView):
     def get(self, request):
         # Only logged in users can update their own streak
@@ -309,10 +343,11 @@ class UserStreakUpdateView(APIView):
             return Response(status=status.HTTP_401_UNAUTHORIZED)
         # Check if the user is active
         User = get_user_model()
-        is_active = User.objects.filter(pk=request.user.id).values('active')[0]["active"]
+        is_active = User.objects.filter(
+            pk=request.user.id).values('active')[0]["active"]
         if is_active:
             return Response(status=status.HTTP_400_BAD_REQUEST)
-        
+
         else:
             request.user.active = True
             request.user.streak += 1
@@ -321,9 +356,10 @@ class UserStreakUpdateView(APIView):
             request.user.save()
             return Response("Successfully Updated")
 
+
 class UserFriendRequestUpdatetView(APIView):
     def post(self, request):
-         # Only logged in users can accept requests
+        # Only logged in users can accept requests
         if not request.user.is_authenticated:
             return Response(status=status.HTTP_401_UNAUTHORIZED)
         if "user_id" not in request.data:
@@ -334,9 +370,10 @@ class UserFriendRequestUpdatetView(APIView):
         request.user.sent_friend_requests.add(id)
         return Response("Successfully Added")
 
+
 class UserFriendRequestAcceptView(APIView):
     def post(self, request):
-         # Only logged in users can accept requests
+        # Only logged in users can accept requests
         if not request.user.is_authenticated:
             return Response(status=status.HTTP_401_UNAUTHORIZED)
         if "user_id" not in request.data:
@@ -348,6 +385,7 @@ class UserFriendRequestAcceptView(APIView):
         request.user.following.add(id)
         request.user.friend_requests.remove(id)
         return Response("Successfully Added")
+
 
 class UserFriendDeleteView(APIView):
     def delete(self, request, pk):
@@ -361,9 +399,10 @@ class UserFriendDeleteView(APIView):
         except Exception as e:
             return Response(str(e), status=status.HTTP_400_BAD_REQUEST)
 
+
 class UserFriendRequestDeclineView(APIView):
     def post(self, request):
-         # Only logged in users can accept requests
+        # Only logged in users can accept requests
         if not request.user.is_authenticated:
             return Response(status=status.HTTP_401_UNAUTHORIZED)
         if "user_id" not in request.data:
@@ -373,6 +412,7 @@ class UserFriendRequestDeclineView(APIView):
             return Response(status=status.HTTP_404_NOT_FOUND)
         request.user.friend_requests.remove(id)
         return Response("Successfully Declined")
+
 
 class UserSearchView(APIView):
     def post(self, request):
@@ -392,6 +432,7 @@ class UserSearchView(APIView):
         serializer = UserSerializer(qs, many=True)
         return Response(serializer.data)
 
+
 def update_community_member_count(num: int, pk: int):
     """
     Function to update community member_count whenever user joins or leaves a community
@@ -400,5 +441,3 @@ def update_community_member_count(num: int, pk: int):
     community = Community.objects.get(pk=pk)
     community.member_count += num
     community.save()
-
-    
