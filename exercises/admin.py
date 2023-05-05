82f19028405472dfbbb53ec7d966af734f23885b
Kuang Yu Heng <yuheng3107@gmail.com>
2023-05-05 09:46:18 +0800

    Registered ExerciseRegimeInfo to admin panel

--- exercises/admin.py
+++ exercises/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Exercise, ExerciseRegime, ExerciseStatistics, ExerciseRegimeStatistics
+from .models import Exercise, ExerciseRegime, ExerciseStatistics, ExerciseRegimeStatistics, ExerciseRegimeInfo
 # Register your models here.
 admin.site.register(Exercise)
 admin.site.register(ExerciseRegime)
 admin.site.register(ExerciseStatistics)
 admin.site.register(ExerciseRegimeStatistics)
+admin.site.register(ExerciseRegimeInfo)
