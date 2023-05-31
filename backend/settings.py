2bdc19c45ceb893a02b79dbbffe0cf10465c250f
Kuang Yu Heng <yuheng3107@gmail.com>
2023-05-31 15:59:22 +0800

    Pls work cors

--- backend/settings.py
+++ backend/settings.py
@@ -176,15 +176,8 @@ USE_TZ = True
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOW_HEADERS = [
-'accept',
-'accept-encoding',
-'authorization',
-'content-type',
-'dnt',
-'origin',
-'user-agent',
-'x-csrftoken',
-'x-requested-with',
+'access-control-allow-origin',
+
 ]
 CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOW_ALL_ORIGINS = True
